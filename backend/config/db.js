e28b2e0c02012a559dd39e9a27b988b7eb1786a2
kobikaradi <kobisagi@gmail.com>
2021-06-02 12:31:21 +0300

    seting up the app

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
     const connection = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 
 //Import Middlewares
 const auth = require("./middleware/auth");
+const path = require("path");
 
 //Import routes
 const productRoutes = require("./routes/productRoutes");
@@ -21,9 +22,19 @@ app.use("/api/register", registerRoute);
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoute);
 
-app.get("/", (req, res) => {
-  res.send("Api is running...");
-});
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api is running...");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(
